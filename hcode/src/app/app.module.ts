794d4357a02fec7a320374beb4525fc8736b118f
eduardo barroca <eduardobarroca2016@gmail.com>
2023-03-05 18:47:35 -0300

    implementação de tela inicial no angular

---
+++ hcode/src/app/app.module.ts
@@ -0,0 +1,68 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+/*importações angular material*/
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    MenuComponent,
+    RodapeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+
+
+    
+    
+
+   
+ ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
